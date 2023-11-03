efaec533b2f58f5bf5fde4d7705b326036730ca0
Clayman <Clayman@DESKTOP-DM8E77U>
2023-11-03 16:42:37 +0300

    Calculations DONE

--- WinAPI/Calc/main.cpp
+++ WinAPI/Calc/main.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<stdio.h>
 #include"resource.h"
 
 CONST CHAR g_sz_CLASSNAME[] = "Calc";
@@ -11,7 +12,7 @@ CONST INT BUTTON_SIZE = 50;
 CONST INT INTERVAL = 5;
 CONST INT BUTTON_DOUBLE_SIZE = BUTTON_SIZE * 2 + INTERVAL;
 
-CONST INT SCREEN_WIDTH = (BUTTON_SIZE) * 5 + INTERVAL*4;
+CONST INT SCREEN_WIDTH = (BUTTON_SIZE) * 5 + INTERVAL * 4;
 CONST INT SCREEN_HEIGHT = 20;
 
 CONST INT BUTTON_START_X = START_X;
@@ -79,6 +80,10 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmsLine, IN
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	static DOUBLE a = 0, b = 0;
+	static INT operation = 0;
+	static BOOL input = false;	//отследивает ввод чисел
+	static BOOL input_operation = false;	//отслеживает ввод операции
 	switch (uMsg)
 	{
 	case WM_CREATE:
@@ -86,7 +91,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HWND hEdit = CreateWindowEx
 		(
 			NULL, "Edit", "Screen",
-			WS_CHILDWINDOW | WS_VISIBLE  | WS_BORDER | ES_RIGHT | ES_READONLY,
+			WS_CHILDWINDOW | WS_VISIBLE | WS_BORDER | ES_RIGHT | ES_READONLY,
 			START_X, START_Y,
 			SCREEN_WIDTH, SCREEN_HEIGHT,
 			hwnd,
@@ -180,9 +185,12 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	break;
 	case WM_COMMAND:
 	{
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+		//					Digits & Point
 		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9 || LOWORD(wParam) == IDC_BUTTON_POINT)
 		{
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+			if (!input)SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
+			input = true;
 			CHAR sz_buffer[MAX_PATH]{};
 			SendMessage(hEdit, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
 			if (strcmp(sz_buffer, "Screen") == 0)
@@ -200,8 +208,53 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			strcat(sz_buffer, sz_digit);
 			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 		}
+		//						Operations:
+		if (LOWORD(wParam) >= IDC_BUTTON_PLUS && LOWORD(wParam) <= IDC_BUTTON_SLASH)
+		{
+			if (input)
+			{
+				CHAR sz_buffer[MAX_PATH]{};
+				SendMessage(hEdit, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+				b = atof(sz_buffer);
+				input = false;
+				if (a == 0)a = b;
+			}
+			if (input_operation)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_EQUAL, 0);
+			operation = LOWORD(wParam);
+			input_operation = true;
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_EQUAL)
+		{
+			if (input)
+			{
+				CHAR sz_buffer[MAX_PATH]{};
+				SendMessage(hEdit, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+				b = atof(sz_buffer);
+				input = false;
+				if (a == 0)a = b;
+			}
+			switch (operation)
+			{
+			case IDC_BUTTON_PLUS:	a += b;	break;
+			case IDC_BUTTON_MINUS:	a -= b;	break;
+			case IDC_BUTTON_ASTER:	a *= b;	break;
+			case IDC_BUTTON_SLASH:	a /= b;	break;
+			}
+			input_operation = false;
+			CHAR sz_buffer[MAX_PATH]{};
+			sprintf(sz_buffer, "%g", a);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_CLEAR)
+		{
+			a = b = 0;
+			operation = 0;
+			input = false;
+			input_operation = false;
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
+		}
 	}
-		break;
+	break;
 	case WM_DESTROY:PostQuitMessage(0); break;
 	case WM_CLOSE:	DestroyWindow(hwnd); break;
 	default:		return DefWindowProc(hwnd, uMsg, wParam, lParam); break;
