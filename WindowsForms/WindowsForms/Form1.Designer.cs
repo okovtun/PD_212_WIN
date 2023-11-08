f883652b6ad3173df17cbbbdaef62e3a752bfca2
Clayman <Clayman@DESKTOP-DM8E77U>
2023-11-08 18:02:10 +0300

    WinForms day 1

---
+++ WindowsForms/WindowsForms/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsForms
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.cbShowDate = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(35, 32);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(192, 73);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Clock";
+			this.label1.DoubleClick += new System.EventHandler(this.label1_DoubleClick);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// cbShowDate
+			// 
+			this.cbShowDate.AutoSize = true;
+			this.cbShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.cbShowDate.Location = new System.Drawing.Point(48, 194);
+			this.cbShowDate.Name = "cbShowDate";
+			this.cbShowDate.Size = new System.Drawing.Size(187, 41);
+			this.cbShowDate.TabIndex = 1;
+			this.cbShowDate.Text = "Show date";
+			this.cbShowDate.UseVisualStyleBackColor = true;
+			this.cbShowDate.Visible = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(626, 344);
+			this.Controls.Add(this.cbShowDate);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.ShowInTaskbar = false;
+			this.Text = "Clock";
+			this.TransparencyKey = System.Drawing.SystemColors.Control;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.CheckBox cbShowDate;
+	}
+}
+
---
+++ WindowsForms/WindowsForms/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			//https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings
+			//https://learn.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings
+			label1.Text = DateTime.Now.ToString("hh:mm:ss tt");
+			if (cbShowDate.Checked) label1.Text += $"\n{DateTime.Now.ToString("dd.MM.yyyy")}";
+		}
+
+		private void label1_DoubleClick(object sender, EventArgs e)
+		{
+			this.FormBorderStyle = FormBorderStyle.Sizable;
+			this.TransparencyKey = Color.AliceBlue;
+			this.ShowInTaskbar = true;
+			this.cbShowDate.Visible = true;
+		}
+	}
+}
